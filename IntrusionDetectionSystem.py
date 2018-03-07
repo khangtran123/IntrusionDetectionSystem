c5653268624f259b153aa8290883a2b5df9406ba
khangtran123 <khangh.tran@hotmail.com>
2018-03-06 23:58:35 -0800

    Update IntrusionDetectionSystem.py

--- IntrusionDetectionSystem.py
+++ IntrusionDetectionSystem.py
@@ -29,6 +29,7 @@ Optional --> Monitor a log file of users choice.
 
 import time
 import os
+import threading
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 #   re --> Python 3 library for regular expression, specifies a set of strings 
@@ -81,12 +82,86 @@ def crontab(attemptsNum, timeSlowScan, banTime):
         crontab.write(command)
         crontab.close()
     #os.system('crontab /etc/crontab')
-    
-def blockIP():
-    return
 
-def unblockIP():
-    return
+'''
+Class userProfile --> is what sets up the UserProfile object that represents
+                      two attributes: Their IP Address + time of attempt
+'''
+class userProfile(object):
+    clientAddress = ""
+    timestamps = []
+    
+    '''
+	__init__ is considered a constructor just like in Java.
+	self represents the instance of that object we declared 
+	this constructor will be our base of what a user should 
+        be made up of --> Their IP Address + all timestamps for 
+        that attempt for that user
+    '''
+        
+    def __init__(self, clientAddress, timestamps):
+        self.clientAddress = clientAddress
+        self.timestamps = timestamps
+
+
+'''
+Function createUserProfile --> pretty much creates a userProfile object by calling
+                               on the userProfile class that creates the user whp
+                               tries to logon to your machine. The class will be
+                               binding their IP address as well as the timestamp
+'''
+def createUserProfile(clientAddress, timestamps):
+    userID = userProfile(clientAddress,timestamps)
+    return userID
+
+
+'''
+Function calculateTotalTime() --> convert the X:X:X timestamp format taken from
+                                  /var/log/secure and convert it to seconds to
+                                  get the total time for each attempt. We need
+                                  to convert it one value (seconds) for useful
+                                  comparison when we calculate the difference in
+                                  time between each attempt.
+'''
+def calculateTotalTime(timeEntry):
+    #   Timestamp format --> HH:MM:SS
+    #   we need to split the stamp into different segments, convert it to 
+    #   seconds, and then add everything together to get the totalTimeAttempt
+    #   1 hour = 3600 seconds
+    #   1 minute = 60 seconds
+    timeSegment = timeEntry.split(':')
+    
+    hour = int(timeSegment[0])
+    hour = hour * 3600
+    minute = int(timeSegment[1])
+    minute = minute * 60
+    second = int(timeSegment[2])
+    
+    totalTimePerAttempt = hour + minute + second
+    
+    return totalTimePerAttempt
+
+def addTimesForUser(timestamp):
+    userID.timestamps.append(timestamp)
+    
+def blockIP(clientIP):
+    global banTime
+    #   Make sure ban time is set to XX minutes
+    if banTime != 0:
+        netFilterCommand = "iptables -A INPUT -s %s -j DROP" % str(clientIP)
+        os.system(netFilterCommand)
+        #   here we are checking for banTime seconds using a thread timer
+        #   more efficient for system handling. Once the time is reached, 
+        #   the thread will call on the unblock function
+        #   threading.timer(Time,function to call, argument)
+        timer = threading.timer(banTime,unblockIP(),clientIP)
+        timer.start()
+        print ("User: " + str(clientIP) + " has been banned from logging into you machine for " + banTime + " minutes")
+
+def unblockIP(clientIP):
+    #   -D --> Removes/Deletes this current netfilter rule
+    removeNetfilterCommand = "iptables -D INPUT -s %s -j DROP" % str(clientIP)
+    os.system(removeNetfilterCommand)
 
 '''
 For this IDS, we will use an event handler to deal with specific IP addresses
@@ -100,9 +175,11 @@ def on_modified --> Called when a file or directory is modified. Soo since
 '''
 
 class Event(LoggingEventHandler):
-    failedAttempts
-    totalAttempts
+    global failedAttempts
+    global totalAttempts
+    #  global bannedClients
     connectionFilepath = "/var/log/secure"
+    #   we set this var as 0 to set the newClient flag off
     
     def on_modified(self, event):
         #   src_path = Source path of the file system object that triggered this event.
@@ -117,13 +194,9 @@ class Event(LoggingEventHandler):
             #   IP address
             lastLFF = line[len(line) - 1]
             secondLFF = line[len(line) - 2]
-            thirdLFF = line[len(line) - 3]
-            fourthLFF = line[len(line) - 4]
+            #thirdLFF = line[len(line) - 3]
+            #fourthLFF = line[len(line) - 4]
             
-            if "more authentication failures" in lastLFF:
-                #lastLFF = thirdLFF
-                #secondLFF = fourthLFF
-                return
             if "Failed password for" in lastLFF:
                 '''
                 findall() --> The string is scanned left-to-right, 
@@ -132,10 +205,56 @@ class Event(LoggingEventHandler):
                 a python "raw" string which passes through backslashes 
                 without change which is very handy for regular expressions
                 '''
+                timestamps = []
                 clientAddress = re.findall(r'[0-9]+(?:\.[0-9]+){3}', lastLFF)
-                timestamp = re.findall()
+                timestamp = re.findall(r'\d{2}:\d{2}:\d{2}', lastLFF)
+                #   if not --> if failedAttempts is [], then execute
+                #   means a new IP Address has failed logging into machine, will always increment for different users
+                if not failedAttemtps:
+                    userID = createUserProfile(clientAddress[0], timestamps)
+                    addTimesForUser(userID, timestamp[0])
+                    print (clientAddress + " attempted to login to your machine. New client is added to the dictionary of failed attempts.")
+                    #  now we want to add this user to our list of failed attempts
+                    failedAttempts.append(userID)
+                    if len(userID.timestamps) > attemptsLimit:
+                        IP = str(userID.clientAddress[0])
+                        blockIP(IP)
+                # That means this is a re-occuring attempt from the same client user
+                else:
+                    newClient = 0
+                    for client in failedAttempts:
+                        if client.clientAddress == clientAddress[0]:
+                            #   client user already exists --> adding timestamp in dictionary
+                            if timestamp[0] not in timestamps:
+                                addTimesForUser(client,timestamp[0])
+                                print ("Existing client --> " + str(clientAddress) + " attempted to login to your machine. New client is added to the dictionary of failed attempts.")
+                                #   now we turn on the newClient flag
+                                newClient = 1
+                                print ("Number of attempts: " + str(attemptsLimit))
+                                if len(client.timestamps) > attemptsLimit:
+                                    timestampsLen = len(client.timestamps)
+                                    firstTimestamp = client.timestamps[(timestampsLen - attemptsLimit)]
+                                    lastTimestamp = client.timestamps[(timestampsLen - 1)]
+                                    firstStamp = calculateTotalTime(firstTimestamp)
+                                    lastStamp = calculateTotalTime(lastTimestamp)
+                                    differenceBetweenFail = (lastStamp - firstStamp)
+                                    
+                                    if differenceBetweenFail <= timeSlowScan:
+                                        IP = str(client.clientAddress)
+                                        blockIP(IP)
+                    if newClient == 0:
+                        userID = createUserProfile(clientAddress[0], timestamps)
+                        addTimesForUser(userID, timestamp[0])
+                        failedAttemps.append(userID)
+                        print (clientAddress + " attempted to login to your machine. New client is added to the dictionary of failed attempts.")
+                        
+            #   if the client gets it right after the failed attempt, this will clear all records of client failed attempts by clearing the timestamps array            
+            elif ("Accepted password for" in lastLFF) or ("Accepted password for" in secondLFF):
+                clientAddress = re.findall(r'[0-9]+(?:\.[0-9]+){3}', secondLFF)
+                for client in incorrectAttempts:
+                    if client.clientAddress == clientAddress[0]:
+                        client.timestamps = []
                 
-
 def main():
     print ("Welcome to our IDS! Unforunately for testing, crontab's limits to only minutes rather than seconds.")
     #attemptsLimit = raw_input("What's the maximum number of failed password attempts you want to set the bar at? ")
@@ -149,7 +268,11 @@ def main():
     
     observer = Observer()
     observer
-    observer.schedule()
-
+    observer.schedule(eventHandler, path='/var/log', recursive=False)
+    observer.start()
+    observer.join()
+    
+    failedAttempts = []
+    
 
 if __name__ == "__main__": main()
